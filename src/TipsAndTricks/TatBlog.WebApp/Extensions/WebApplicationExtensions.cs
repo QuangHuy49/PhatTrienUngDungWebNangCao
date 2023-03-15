eafbf4d9c5c8a8d9ce3f7f3468c360553ba82640
Quang Huy <quanghuy2707ht@gmail.com>
2023-03-15 18:46:30 +0700

    Hoàn thành phần 6 Homework

--- src/TipsAndTricks/TatBlog.WebApp/Extensions/WebApplicationExtensions.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Extensions/WebApplicationExtensions.cs
@@ -2,6 +2,7 @@
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
+using TatBlog.Services.Media;
 
 namespace TatBlog.WebApp.Extensions
 {
@@ -16,11 +17,13 @@ namespace TatBlog.WebApp.Extensions
         }
 
         //Đăng ký các dịch vụ với DI Container
-        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        public static WebApplicationBuilder ConfigureServices(
+            this WebApplicationBuilder builder)
         {
             builder.Services.AddDbContext<BlogDbContext>(options =>
             options.UseSqlServer(
                 builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddScoped<IMediaManager, LocalFileSystemMediaManager>();
             builder.Services.AddScoped<IBlogRepository, BlogRepository>();
             builder.Services.AddScoped<IDataSeeder, DataSeeder>();
             return builder;
